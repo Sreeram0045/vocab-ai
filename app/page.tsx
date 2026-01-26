842e5d117573f11d168582eb210821717bdf1e42
flyme2mars <akshai404akz@gmail.com>
2026-01-26 23:31:36 +0530

    Added intro animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-
 import { auth } from "@/auth";
 import VocabClient from "@/components/vocab-client";
+import WelcomeScreen from "@/components/intro/welcome-screen";
 
 export const dynamic = "force-dynamic";
 
@@ -9,6 +9,7 @@ export default async function Home() {
 
   return (
     <div className="min-h-screen bg-background text-foreground font-sans flex flex-col items-center overflow-x-hidden">
+      <WelcomeScreen />
       <VocabClient user={session?.user} />
     </div>
   );
